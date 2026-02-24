f143e8e1473e695b375c283c813252586f3ea844
Rashed Khan <118573694+RashedKhanSezan@users.noreply.github.com>
2026-02-25 00:56:46 +0600

    Add responsive layout example in flexible.dart
    Implement a Flutter app with a FractionallySizedBox inside a Flexible widget to demonstrate responsive design.

---
+++ responsive/flexible.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('FractionallySizedBox Example'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Flexible(
+                child: FractionallySizedBox(
+                  heightFactor:
+                      .7, //set a constrain 70% of height , so flexible takes 70% height
+                  widthFactor: 0.2,
+                  child: Container(
+                    color: Colors.blueAccent,
+                    child: const Center(
+                      child: Text(
+                        'Rashed',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Text('Rashedkhansezan')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
